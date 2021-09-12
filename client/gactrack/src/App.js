eb0d05c11f1008e349decf718174d3684fb009fe
anudaweerasinghe <anuda@live.com>
2021-09-11 23:49:31 -0400

    made background image less opaque

--- client/gactrack/src/App.js
+++ client/gactrack/src/App.js
@@ -9,11 +9,21 @@ function App() {
     <div
       className="App"
       style={{
-        backgroundImage: "url('https://source.unsplash.com/random/1920x1080')",
-        height: "100vh",
+        backgroundImage:
+          "linear-gradient(rgba(255,255,255,0.2), rgba(255,255,255,0.2)), url('https://source.unsplash.com/random/1920x1080')",
       }}
     >
-      <Time />
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Time />
+        </Grid>
+      </Grid>
     </div>
   );
 }
